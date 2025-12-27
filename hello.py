692330fdf7eb8dc016bc312bcf62e857d59e8074
Mohit-72in <mohityadav950410s@gmail.com>
2025-12-27 14:53:13 +0530

    adding 1st py file

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("My first Python program!") 
+
+class Student:
+    name = "Mohit Yadav "
+    age = 23
+    college = "IIT Patna"
+    branch = "CSE"
+
+student1 = Student()
+student2 = Student()
+print(student1.name)
+print(student2.college)     
+print(student2.branch)
